d3d6fb53a60900d2f243793be3a9dc1338186f1a
zhuchunshu <laravel@88.com>
2021-06-09 21:41:04 +0800

    更新:记录状态

--- src/Jobs/HtTask.php
+++ src/Jobs/HtTask.php
@@ -2,6 +2,7 @@
 
 namespace App\Plugins\Htshop\src\Jobs;
 
+use App\Plugins\Htshop\src\Models\Htshop;
 use Illuminate\Support\Arr;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
@@ -37,37 +38,52 @@ class HtTask implements ShouldQueue
      */
     public function handle()
     {
-        $response =http_gets($this->config["任务列表"]);
-        $data = $response->json();
-        // 商品列表
-        $shopList = http_gets("https://www.heytap.com/cn/oapi/goods/web/ranking/rankDetailInfo?rankId=2240&currentPage=1&pageSize=100")->json();
-        if ($data['no'] == 200) {
-            foreach ($data['data'] as $value) {
-                //做任务
-                htcurl_post($this->config['完成浏览任务'], $this->htshop->cookies, [
-                    "aid" => 1506,
-                    "t_index" => $value['t_index']
-                ]);
-
-                if ($value['title'] == "浏览商品") {
-                    for ($i = 0; $i < 6; $i++) {
-                        htcurl_get($this->config["浏览商品"], $this->htshop->cookies, [
-                            "skuId" => Arr::random($shopList['data']['rankingGoods'])['goodsSkuId']
-                        ]);
-                        // 间隔2秒
-                        sleep(5);
+        $htp = http_getsWithHeaders("https://www.heytap.com/cn/oapi/users/web/member/info",[
+            "Cookie"=>$this->htshop->cookies,
+            "Accept"=>"application/json, text/plain, */*"
+        ])->json();
+        if($htp['code']==200){
+            Htshop::where('id',$this->htshop->id)->update([
+                "status" => "成功,accountName:".$htp['data']['accountName'].",id:".$htp['data']['id']
+            ]);
+            $response =http_gets($this->config["任务列表"]);
+            $data = $response->json();
+            // 商品列表
+            $shopList = http_gets("https://www.heytap.com/cn/oapi/goods/web/ranking/rankDetailInfo?rankId=2240&currentPage=1&pageSize=100")->json();
+            if ($data['no'] == 200) {
+                foreach ($data['data'] as $value) {
+                    //做任务
+                    htcurl_post($this->config['完成浏览任务'], $this->htshop->cookies, [
+                        "aid" => 1506,
+                        "t_index" => $value['t_index']
+                    ]);
+    
+                    if ($value['title'] == "浏览商品") {
+                        for ($i = 0; $i < 6; $i++) {
+                            htcurl_get($this->config["浏览商品"], $this->htshop->cookies, [
+                                "skuId" => Arr::random($shopList['data']['rankingGoods'])['goodsSkuId']
+                            ]);
+                            // 间隔2秒
+                            sleep(5);
+                        }
                     }
+    
+                    // 自动领取
+                    htcurl_post($this->config['领积分'], $this->htshop->cookies, [
+                        "aid" => 1506,
+                        "t_index" => $value['t_index']
+                    ]);
+                    
                 }
-
-                // 自动领取
-                htcurl_post($this->config['领积分'], $this->htshop->cookies, [
-                    "aid" => 1506,
-                    "t_index" => $value['t_index']
-                ]);
-                
+            } else {
+                dd("出错: 任务ID:" . $this->htshop->id);
             }
-        } else {
-            return("出错: 任务ID:" . $this->htshop->id);
+        }else{
+            Htshop::where('id',$this->htshop->id)->update([
+                "status" => "出错"
+            ]);
+            dd("出错");
         }
+        
     }
 }
