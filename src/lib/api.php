7a33dacd393c00a8c504588f87caa481422289ee
zhuchunshu <laravel@88.com>
2021-06-09 12:16:19 +0800

    更新:定义各接口地址

---
+++ src/lib/api.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "签到" => "https://store.oppo.com/cn/oapi/credits/web/report/immediately",
+    "任务列表" => "https://hd.oppo.com/task/list?aid=1506",
+    "领券" => "https://hd.oppo.com/platform/receive",
+    "完成浏览任务" => "https://hd.oppo.com/task/finish",
+    "领积分" => "https://hd.oppo.com/task/award"
+];
