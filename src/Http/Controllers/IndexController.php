73687b2eb142a13a72162b5953c47801862f754a
zhuchunshu <laravel@88.com>
2021-06-10 15:34:24 +0800

    更新:自动运行数据库迁移

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -2,8 +2,10 @@
 namespace App\Plugins\Htshop\src\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use App\Plugins\Htshop\src\Http\Requests\CreateRequest;
+use Illuminate\Support\Facades\Schema;
 use App\Plugins\Htshop\src\Models\Htshop;
+use App\Plugins\Htshop\src\database\CreateHtshopTable;
+use App\Plugins\Htshop\src\Http\Requests\CreateRequest;
 
 class IndexController {
 
@@ -11,6 +13,10 @@ class IndexController {
     public function show(){
         if(!get_options_setting("htshop")){
             CodeFec_Quanxian(999);
+        }
+        if (!Schema::hasTable('htshop')) {
+            CreateHtshopTable::up();
+            return redirect()->route("Htshop.index")->with("success","数据库迁移成功!");
         }
         return view("Htshop::index");
     }
